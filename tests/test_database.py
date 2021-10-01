4138718c398c4d6df713e4495406f3251acd66c0
machikoyasuda <machiko@compiler.la>
2021-10-01 18:59:23 +0000

    tests(database): lint

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,48 +3,33 @@ Test database class and methods
 """
 import json
 
-with open("data/server.json") as f:
-  DATA = json.load(f)
-
-def test_database_init(database):
-  assert database._config
-  assert database._merchants
-  assert database._users
-
-def test_database_properties(database):
-  assert database.auth_header
-  assert database.auth_token
-  assert database.token_header
-  assert database.jwe_cek_enc
-  assert database.jwe_encryption_alg
-  assert database.jws_signing_alg
-  assert database.request_access
+with open("data/server.json", encoding="utf8") as file:
+    DATA = json.load(file)
 
 def test_database_check_merchant_in_database(database):
-  merchant_id = DATA['merchants'][1]
-  response = database.check_merchant(merchant_id)
-  assert response == True
+    merchant_id = DATA['merchants'][1]
+    response = database.check_merchant(merchant_id)
+    assert response is True
 
 def test_database_check_merchant_not_in_database(database):
-  merchant_id_not_in_database = '123'
-  response = database.check_merchant(merchant_id_not_in_database)
-  assert response == False
+    merchant_id_not_in_database = '123'
+    response = database.check_merchant(merchant_id_not_in_database)
+    assert response is False
 
 def test_database_check_user_in_database(database):
-  key = min(DATA['users'])
-  user = DATA['users'][key][0]
-  types = DATA['users'][key][1]
-  response = database.check_user(key, user, types)
-  assert response == types
+    key = min(DATA['users'])
+    user = DATA['users'][key][0]
+    types = DATA['users'][key][1]
+    response = database.check_user(key, user, types)
+    assert response == types
 
 def test_database_check_user_in_database_not_eligible(database):
-  key = min(DATA['users'])
-  user = DATA['users'][key][0]
-  types = ['type2']
-  response = database.check_user(key, user, types)
-  assert response == []
+    key = min(DATA['users'])
+    user = DATA['users'][key][0]
+    types = ['type2']
+    response = database.check_user(key, user, types)
+    assert response == []
 
 def test_database_check_user_not_in_database(database):
-  response = database.check_user('G7778889', 'Thomas', ['type1'])
-  assert response == []
-
+    response = database.check_user('G7778889', 'Thomas', ['type1'])
+    assert response == []

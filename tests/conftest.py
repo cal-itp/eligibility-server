055039b33a26e75dafb14d1340c8ac846a2d6280
machikoyasuda <machiko@compiler.la>
2021-10-01 22:21:50 +0000

    test(database): add database unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,18 @@
 import pytest
 
 from eligibility_server.app import app as server
+from app import Database as Database
 
 
 @pytest.fixture
 def app():
     yield server
+
+
+@pytest.fixture
+def database():
+    db = Database()
+    return db
 
 
 @pytest.fixture

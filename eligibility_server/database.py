e9765879b54c867a13f48a54780e0be6ac35ea83
machikoyasuda <machiko@compiler.la>
2021-12-15 01:37:24 +0000

    feat(database): refactor check_user to optionally hash key and user inputs

--- eligibility_server/database.py
+++ eligibility_server/database.py
@@ -3,6 +3,7 @@ Simple hard-coded server database.
 """
 
 import json
+import hashlib
 
 
 class Database:
@@ -11,14 +12,43 @@ class Database:
             data = json.load(f)
             self._users = data["users"]
 
-    def check_user(self, key, user, types):
-        """Check if the data matches a record in the database."""
+    def hash_input(self, input):
+        """
+        Return a SHA-256 hash of inputted string
+
+        @param input: string
+
+        @return string
+        """
+
+        return hashlib.sha256(input.encode("utf-8")).hexdigest()
+
+    def check_user(self, key, user, types, hash_inputs=False):
+        """
+        Check if the data matches a record in the database, optionally, check with hashed key and user strings.
+
+        @param self: self
+        @param key: string - key to check for
+        @param user: string - name of user to check for
+        @param types: string - type of eligibility
+        @param hash_inputs: boolean - default False, defaults to not hashing inputs
+
+        @return boolean
+        """
+
+        key_to_check = key
+        user_to_check = user
+
+        if hash_inputs:
+            key_to_check = self.hash_input(key)
+            user_to_check = self.hash_input(user)
+
         if (
             len(types) < 1
-            or key not in self._users
-            or self._users[key][0] != user
-            or len(set(self._users[key][1]) & set(types)) < 1
+            or key_to_check not in self._users
+            or self._users[key_to_check][0] != user_to_check
+            or len(set(self._users[key_to_check][1]) & set(types)) < 1
         ):
             return []
 
-        return list(set(self._users[key][1]) & set(types))
+        return list(set(self._users[key_to_check][1]) & set(types))
--- tests/test_database.py
+++ tests/test_database.py
@@ -16,7 +16,9 @@ def test_database_check_user_in_database(database):
     key = min(DATA["users"])
     user = DATA["users"][key][0]
     types = DATA["users"][key][1]
+
     response = database.check_user(key, user, types)
+
     assert response == types
 
 
@@ -24,10 +26,23 @@ def test_database_check_user_in_database_not_eligible(database):
     key = min(DATA["users"])
     user = DATA["users"][key][0]
     types = ["type2"]
-    response = database.check_user(key, user, types)
+
+    response = database.check_user(key, user, types, False)
+
     assert response == []
 
 
 def test_database_check_user_not_in_database(database):
     response = database.check_user("G7778889", "Thomas", ["type1"])
+
     assert response == []
+
+
+def test_database_check_user_in_database_with_hashing(database):
+    key = "A1234568"
+    user = "Garcia"
+    types = ["type1"]
+
+    response = database.check_user(key, user, types, True)
+
+    assert response == types

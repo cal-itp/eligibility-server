8f208dc8d80b5df14cfa155251e0a6decee82358
machikoyasuda <machiko@compiler.la>
2022-01-14 04:57:20 +0000

    test(hash): reformat parameters

--- tests/test_hash.py
+++ tests/test_hash.py
@@ -7,13 +7,13 @@ from eligibility_server.hash import Hash
 
 
 test_data = [
-    (Hash("sha256").hash_input("Test"), "532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25"),
+    ("sha256", "532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25"),
     (
-        Hash("sha512").hash_input("Test"),
+        "sha512",
         "c6ee9e33cf5c6715a1d148fd73f7318884b41adcb916021e2bc0e800a5c5dd97f5142178f6ae88c8fdd98e1afb0ce4c8d2c54b5f37b30b7da1997bb33b0b8a31",  # noqa: E501
     ),
     (
-        Hash("whirlpool").hash_input("Test"),
+        "whirlpool",
         "ac1c196e84b05d59faea639d0cccb6c16f7568e0943f4c94c648bd8eefe5cf3978f8d4f99b1426e4ce3c6fa40e17cf68d6b86d42246333569b963ff4579c6355",  # noqa: E501
     ),
 ]
@@ -33,6 +33,6 @@ def test_hash_init_invalid_blank():
         Hash()
 
 
-@pytest.mark.parametrize("input, expected", test_data)
-def test_hash_input(input, expected):
-    assert input == expected
+@pytest.mark.parametrize("hash_input_algo, expected", test_data)
+def test_hash_input(hash_input_algo, expected):
+    assert Hash(hash_input_algo).hash_input("Test") == expected

f2a24f9dd526485f4c60111a5e7b651e364ee9e6
machikoyasuda <machiko@compiler.la>
2022-01-27 21:48:32 +0000

    fix: remove string size

--- eligibility_server/app.py
+++ eligibility_server/app.py
@@ -29,9 +29,9 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(120), unique=True, nullable=False)
-    key = db.Column(db.String(120), unique=True, nullable=False)
-    types = db.Column(db.String(120), unique=False, nullable=False)
+    user_id = db.Column(db.String, unique=True, nullable=False)
+    key = db.Column(db.String, unique=True, nullable=False)
+    types = db.Column(db.String, unique=False, nullable=False)
 
     def __repr__(self):
         return "<User %r>" % self.user_id

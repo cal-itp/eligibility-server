9dc40d7c8af67199ba992393d3afdaadb586606a
machikoyasuda <machiko@compiler.la>
2022-02-08 18:46:27 +0000

    refactor: remove global variables, add type casting, add docs

--- setup.py
+++ setup.py
@@ -3,33 +3,38 @@ import json
 
 from eligibility_server import app, settings
 
-db = app.db
-User = app.User
 
-
-def import_users(db, User):
+def import_users():
     print("Importing users from", settings.IMPORT_FILE_PATH)
     if settings.IMPORT_FILE_FORMAT == "json":
         with open(settings.IMPORT_FILE_PATH) as file:
             data = json.load(file)["users"]
             for user in data:
-                save_users(db, User, user, data[user][0], str(data[user][1]))
+                save_users(user, data[user][0], str(data[user][1]))
     if settings.IMPORT_FILE_FORMAT == "csv":
         with open(settings.IMPORT_FILE_PATH, newline="", encoding="utf-8") as file:
             data = csv.reader(file, delimiter=";", quotechar="", quoting=csv.QUOTE_NONE)
             for user in data:
-                save_users(db, User, user[0], user[1], user[2])
+                save_users(user[0], user[1], user[2])
 
 
-def save_users(db, User, user_id, key, types):
-    item = User(user_id=user_id, key=key, types=types)
-    db.session.add(item)
-    db.session.commit()
+def save_users(user_id: str, key: str, types: str):
+    """
+    Add users to the database User table
+
+    @param user_id - User's ID, not to be confused with Database row ID
+    @param key - User's key
+    @param types - Types of eligibilities, in a stringified list
+    """
+
+    item = app.User(user_id=user_id, key=key, types=types)
+    app.db.session.add(item)
+    app.db.session.commit()
 
 
 if __name__ == "__main__":
     print("Creating table...")
-    db.create_all()
+    app.db.create_all()
     print("Table created.")
-    import_users(db, User)
-    print(User.query.count(), "users added.")
+    import_users(app.db, app.User)
+    print(app.User.query.count(), "users added.")
--- teardown.py
+++ teardown.py
@@ -1,11 +1,8 @@
 from eligibility_server import app
 
-db = app.db
-User = app.User
-
 if __name__ == "__main__":
-    print(User.query.count(), "users to be deleted.")
-    User.query.delete()
-    db.session.commit()
-    db.drop_all()
+    print(app.User.query.count(), "users to be deleted.")
+    app.User.query.delete()
+    app.db.session.commit()
+    app.db.drop_all()
     print("Database dropped.")

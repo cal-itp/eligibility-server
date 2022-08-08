030a26fa57c124f120dfd3023680465a8a1dff58
Machiko Yasuda <machiko@compiler.la>
2022-08-08 22:00:47 +0000

    feat(logs): add line number and name

--- eligibility_server/app.py
+++ eligibility_server/app.py
@@ -15,7 +15,7 @@ dictConfig(
         "version": 1,
         "formatters": {
             "default": {
-                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+                "format": "[%(asctime)s] %(levelname)s %(name)s:%(lineno)s %(message)s",
             }
         },
         "handlers": {

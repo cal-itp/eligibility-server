2ccefa334abc859a78fd323caa0b68953cf2ef72
Machiko Yasuda <machiko@compiler.la>
2022-08-24 20:13:59 +0000

    test(verify): refactor tests to make API calls to the client, rather than calling the method directly

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -1,36 +1,28 @@
 import json
 import uuid
-import pytest
 
 from eligibility_server.verify import Verify
 
 
-def test_Verify_get_unauthorized():
-    with pytest.raises(Exception) as e:
+def test_Verify_client_get_unauthorized_request(client):
+    response = client.get("/verify")
 
-        def get_without_headers():
-            # GET request missing header
-            response = Verify().get()
-            return response
-
-        get_without_headers()
-
-    assert "403 Forbidden: Unauthorized" in str(e.value)
+    assert response.status_code == 403
+    assert response.content_type == "application/json"
+    assert response.json["message"] == "Unauthorized"
 
 
-def test_Verify_get_bad_request(mocker):
-    with pytest.raises(Exception) as e:
+def test_Verify_client_get_bad_token(mocker, client):
+    token_payload = dict(sub="A1234567", name="Garcia", eligibility=["type1"])
+    mocked_headers = {"TOKEN_HEADER": "blah"}
+    mocker.patch("eligibility_server.verify.Verify._check_headers", return_value=mocked_headers)
+    mocker.patch("eligibility_server.verify.Verify._get_token", return_value="token")
+    mocker.patch("eligibility_server.verify.Verify._get_token_payload", return_value="bad token")
+    response = client.get("/verify", json=token_payload)
 
-        def get_with_bad_token():
-            mocker.patch("eligibility_server.verify.Verify._make_token", return_value="token")
-            # Token Payload is missing JTI
-            token_payload = json.loads(json.dumps(dict(sub="A1234567", name="Garcia", eligibility=["type1"])))
-            response = Verify()._get_response(token_payload)
-            return response
-
-        get_with_bad_token()
-
-    assert "500 Internal Server Error" in str(e.value)
+    assert response.status_code == 500
+    assert response.content_type == "application/json"
+    assert response.json["message"].startswith("Internal server error:")
 
 
 def test_Verify_get_response_sub_format_match(mocker, mock_config):

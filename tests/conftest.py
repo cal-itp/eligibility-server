c66b81662340299e9c65de21e4dd95aedbb39cf3
Kegan Maher <kegan@compiler.la>
2022-08-24 20:53:43 +0000

    fix(tests): use patch.dict helper on app.config
    this maintains app.config as a dict-like object instead of
    a mock object, preventing an error with reading the keypair
    config on Verify init that was causing these tests to fail;
    remove now unused fixture
    
    remove unnecessary mock: these tests are
    testing _get_response which doesn't need the _get_token helper
    
    clarify return/assert value by unpacking tuple

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,16 +11,3 @@ def flask():
 @pytest.fixture
 def client():
     return app.test_client()
-
-
-@pytest.fixture
-def mock_config(mocker):
-    def get_mocked_config(custom_config_dict):
-        config = {k: v for (k, v) in app.config.items()}
-        config.update(custom_config_dict)
-
-        mock = mocker.MagicMock()
-        mock.__getitem__.side_effect = config.__getitem__
-        return mock
-
-    return get_mocked_config
--- tests/test_verify.py
+++ tests/test_verify.py
@@ -25,24 +25,24 @@ def test_Verify_client_get_bad_request(mocker, client):
     assert response.json["message"].startswith("Bad request")
 
 
-def test_Verify_get_response_sub_format_match(mocker, mock_config):
-    mocked_config = mock_config({"SUB_FORMAT_REGEX": r"^[A-Z]\d{7}$", "INPUT_HASH_ALGO": ""})
-    mocker.patch("eligibility_server.verify.app.app.config", new=mocked_config)
-    mocker.patch("eligibility_server.verify.Verify._make_token", return_value="token")
+def test_Verify_get_response_sub_format_match(mocker):
+    mocked_config = {"SUB_FORMAT_REGEX": r"^[A-Z]\d{7}$", "INPUT_HASH_ALGO": ""}
+    mocker.patch.dict("eligibility_server.verify.app.app.config", mocked_config)
+
     token_payload = json.loads(json.dumps(dict(sub="A1234567", name="Garcia", eligibility=["type1"], jti=str(uuid.uuid4()))))
 
-    response = Verify()._get_response(token_payload)
+    _, response_code = Verify()._get_response(token_payload)
 
-    assert response[1] == 200
+    assert response_code == 200
 
 
-def test_Verify_get_response_sub_format_no_match(mocker, mock_config):
-    mocked_config = mock_config({"SUB_FORMAT_REGEX": r"^[A-Z]\d{7}$", "INPUT_HASH_ALGO": ""})
-    mocker.patch("eligibility_server.verify.app.app.config", new=mocked_config)
-    mocker.patch("eligibility_server.verify.Verify._make_token", return_value="token")
+def test_Verify_get_response_sub_format_no_match(mocker):
+    mocked_config = {"SUB_FORMAT_REGEX": r"^[A-Z]\d{7}$", "INPUT_HASH_ALGO": ""}
+    mocker.patch.dict("eligibility_server.verify.app.app.config", mocked_config)
+
     # "sub" value does not match the format regex
     token_payload = json.loads(json.dumps(dict(sub="nomatch", name="Garcia", eligibility=["type1"], jti=str(uuid.uuid4()))))
 
-    response = Verify()._get_response(token_payload)
+    _, response_code = Verify()._get_response(token_payload)
 
-    assert response[1] == 400
+    assert response_code == 400

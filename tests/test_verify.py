e201a53c312b3b67f9b762bca31eb698f8b1748d
Machiko Yasuda <machiko@compiler.la>
2022-08-25 21:36:58 +0000

    chore: remove ??

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -22,7 +22,7 @@ def test_Verify_client_get_bad_request(mocker, client):
 
     assert response.status_code == 400
     assert response.content_type == "application/json"
-    assert response.json["message"].startswith("Bad request??")
+    assert response.json["message"].startswith("Bad request")
 
 
 def test_Verify_get_response_sub_format_match(mocker, mock_config):

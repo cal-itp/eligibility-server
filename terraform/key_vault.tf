6915ecd06031b6405d5ad3a0ed8382d4fa552a69
Angela Tran <angela@compiler.la>
2022-10-31 21:05:28 +0000

    chore(terraform): set access policies for key vault

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -5,6 +5,17 @@ resource "azurerm_key_vault" "main" {
   sku_name            = "standard"
   tenant_id           = data.azurerm_client_config.current.tenant_id
 
+  # we need this to set secrets through the portal
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get",
+      "Set"
+    ]
+  }
+
   lifecycle {
     prevent_destroy = true
     ignore_changes  = [tags]

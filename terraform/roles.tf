df767f223207374b0935ae812f1b76e83a9341a5
Angela Tran <angela@compiler.la>
2022-11-01 19:30:51 +0000

    refactor(terraform): only create the Velocity ETL role for prod

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -1,4 +1,7 @@
+# this role only applies to prod since Velocity ETL is only uploading to prod
 resource "azurerm_role_definition" "velocity_etl" {
+  count       = local.is_prod ? 1 : 0
+
   name        = "Velocity ETL"
   scope       = azurerm_storage_container.config.resource_manager_id
   description = "This is a custom role created via Terraform for the Velocity ETL service principal to upload the hashed data file"
@@ -9,6 +12,8 @@ resource "azurerm_role_definition" "velocity_etl" {
 }
 
 # resource "azurerm_role_assignment" "velocity_etl" {
+#   count       = local.is_prod ? 1 : 0
+#
 #   description = "This role assignment gives write access only for the path of the hashed data file."
 #   scope              = azurerm_storage_container.config.resource_manager_id
 #   role_definition_id = azurerm_role_definition.velocity_etl.role_definition_resource_id

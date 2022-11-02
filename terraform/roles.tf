afd54060dc47bc8457cdb9358762db2d707e2433
Angela Tran <angela@compiler.la>
2022-11-02 15:22:18 +0000

    refactor(terraform): use built-in role for assignment instead

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -1,33 +1,16 @@
-# this role only applies to prod since Velocity ETL is only uploading to prod
-resource "azurerm_role_definition" "velocity_etl" {
-  count       = local.is_prod ? 1 : 0
+resource "azurerm_role_assignment" "velocity_etl" {
+  count = local.is_prod ? 1 : 0
 
-  name        = "Velocity ETL"
-  scope       = azurerm_storage_container.config.resource_manager_id
-  description = "This is a custom role created via Terraform for the Velocity ETL service principal to upload the hashed data file"
-
-  permissions {
-    data_actions = ["Microsoft.Storage/storageAccounts/blobServices/containers/blobs/write"]
-  }
+  description          = "This role assignment gives write access only for the path of the hashed data file."
+  scope                = azurerm_storage_container.config.resource_manager_id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = var.velocity_etl_service_principal_id
+  condition            = <<EOF
+(
+ (
+  @Resource[Microsoft.Storage/storageAccounts/blobServices/containers/blobs:path] StringLike 'velocity.csv'
+ )
+)
+EOF
+  condition_version    = "2.0"
 }
-
-# resource "azurerm_role_assignment" "velocity_etl" {
-#   count       = local.is_prod ? 1 : 0
-#
-#   description = "This role assignment gives write access only for the path of the hashed data file."
-#   scope              = azurerm_storage_container.config.resource_manager_id
-#   role_definition_id = azurerm_role_definition.velocity_etl.role_definition_resource_id
-#   principal_id       = "todo"
-#   condition          = <<EOF
-# (
-#  (
-#   !(ActionMatches{'Microsoft.Storage/storageAccounts/blobServices/containers/blobs/write'})
-#  )
-#  OR
-#  (
-#   @Resource[Microsoft.Storage/storageAccounts/blobServices/containers/blobs:path] StringEquals 'velocity.csv'
-#  )
-# )
-# EOF
-#   condition_version = "2.0"
-# }
---
+++ terraform/variables.tf
@@ -0,0 +1,3 @@
+variable "velocity_etl_service_principal_id" {
+  description = "Object ID from the registered application"
+}

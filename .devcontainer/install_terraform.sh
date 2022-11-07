5cff40e8e9eaa2c25cde0b8915d2f22545edd508
Aidan Feldman <aidan.feldman@gmail.com>
2022-11-07 23:10:33 +0000

    chore: check Terraform formatting as part of pre-commit

---
+++ .devcontainer/install_terraform.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -x
+
+apt-get update
+apt-get install -y lsb-release
+
+# based on
+# https://developer.hashicorp.com/terraform/downloads
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+apt-get update
+apt-get install terraform

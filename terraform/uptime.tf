8f8fc938c57b004e4eda5ea8b6ab0c63a277c897
Aidan Feldman <aidan@compiler.la>
2023-02-16 11:02:16 -0500

    fix(Azure): suppress downtime alerts when data is being loaded

--- terraform/uptime.tf
+++ terraform/uptime.tf
@@ -8,3 +8,27 @@ module "healthcheck" {
   name = "mst-courtesy-cards-eligibility-server-${local.env_name}-healthcheck"
   url  = "https://${azurerm_linux_web_app.main.default_hostname}/healthcheck"
 }
+
+# ignore when app restarts as data is being reloaded
+# https://learn.microsoft.com/en-us/azure/azure-monitor/alerts/alerts-processing-rules
+resource "azurerm_monitor_action_rule_suppression" "suppression" {
+  name                = "ignore-data-loading"
+  resource_group_name = data.azurerm_resource_group.main.name
+
+  scope {
+    type         = "Resource"
+    resource_ids = [module.healthcheck.metric_alert_id]
+
+  }
+
+  suppression {
+    recurrence_type = "Daily"
+
+    schedule {
+      start_date_utc = "2020-01-01T00:00:00Z"
+      end_date_utc   = "2050-01-01T00:00:00Z"
+      start_time_utc = "11:00AM"
+      end_time_utc   = "11:10AM"
+    }
+  }
+}

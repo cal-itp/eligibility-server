c8d347da019904806b75b16ade9d40ab43f4b30c
Angela Tran <angela@compiler.la>
2023-08-17 20:33:21 +0000

    feat: set variable to indicate run is for a tag, update workspace logic

--- terraform/pipeline/workspace.py
+++ terraform/pipeline/workspace.py
@@ -1,10 +1,12 @@
 import os
+import re
 import sys
 
 REASON = os.environ["REASON"]
 # the name of the variable that Azure Pipelines uses for the source branch depends on the type of run, so need to check both
 SOURCE = os.environ.get("OTHER_SOURCE") or os.environ["INDIVIDUAL_SOURCE"]
 TARGET = os.environ["TARGET"]
+IS_TAG = os.environ["IS_TAG"]
 
 # the branches that correspond to environments
 ENV_BRANCHES = ["dev", "test", "prod"]
@@ -15,6 +17,8 @@ if REASON == "PullRequest" and TARGET in ENV_BRANCHES:
 elif REASON in ["IndividualCI", "Manual"] and SOURCE in ENV_BRANCHES:
     # it's being run on one of the environment branches, so use that
     environment = SOURCE
+elif REASON in ["IndividualCI"] and IS_TAG and re.fullmatch(r"20\d\d.\d\d.\d*", SOURCE):
+    environment = "test"
 else:
     # default to running against dev
     environment = "dev"

29c155b3de17f3f82ba3da100ba0995b7f3cc03e
Angela Tran <angela@compiler.la>
2023-08-25 20:09:35 +0000

    feat: use small Terraform configuration for experimenting

---
+++ experiment/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  // see version in experiment-pipeline.yml
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0, < 4.0.0"
+    }
+  }
+
+  backend "azurerm" {
+    # needs to match experiment-pipeline.yml
+    resource_group_name  = "experiment-terraform"
+    storage_account_name = "experimentterraform"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}

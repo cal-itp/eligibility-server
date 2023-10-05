8af3d3252eab1b35e78375e94fc44a7760089157
Angela Tran <angela@compiler.la>
2023-10-05 17:13:44 +0000

    feat(terraform): create agency-specific backend config for local dev
    the pipeline doesn't need the backend config block in main.tf

--- terraform/init.sh
+++ terraform/init.sh
@@ -3,19 +3,21 @@
 set -e
 
 
-ENV=$1
-SUBSCRIPTION=$2
+ENV="$1"
+AGENCY="$2"
 
 if [ $# -ne 2 ]; then
-  echo "Usage: $0 <env> <subscription name>"
+  echo "Usage: $0 <env> <agency>"
   exit 1
 fi
+
+source "$AGENCY/env"
 
 echo "Setting the subscription for the Azure CLI..."
 az account set --subscription="$SUBSCRIPTION"
 
 printf "Intializing Terraform...\n\n"
-terraform init
+terraform init -backend-config="$AGENCY/config.azurerm.tfbackend"
 
 printf "\n\nSelecting the Terraform workspace...\n"
 # matching logic in pipeline/workspace.py

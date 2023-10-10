50fbd286fa8224e4f0381f5a894722b3c699d283
Angela Tran <angela@compiler.la>
2023-10-10 22:56:15 +0000

    chore: make description for ETL app object ID variable more generic

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,7 +9,7 @@ variable "AGENCY_CARD" {
 }
 
 variable "AGENCY_CARD_DATA_ETL_APP_OBJECT_ID" {
-  description = "Object ID from the registered application for the Velocity server ETL uploading: https://cloudsight.zendesk.com/hc/en-us/articles/360016785598-Azure-finding-your-service-principal-object-ID"
+  description = "Object ID from the registered application for the Agency Card server ETL uploading: https://cloudsight.zendesk.com/hc/en-us/articles/360016785598-Azure-finding-your-service-principal-object-ID"
   type        = string
 }
 
